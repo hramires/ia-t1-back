61eb20ccf3676ac553f5cccf5ff83eb6ec1e2efb
yazuc <51753599+yazuc@users.noreply.github.com>
2023-05-09 17:15:24 -0300

    Mlp service quase funcional

--- src/main/java/com/pucrs/iat1back/mlp/service/MlpService.java
+++ src/main/java/com/pucrs/iat1back/mlp/service/MlpService.java
@@ -2,75 +2,98 @@ package com.pucrs.iat1back.mlp.service;
 
 import com.pucrs.iat1back.dto.CalculoDTO;
 import com.pucrs.iat1back.enumerator.StatusEnum;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.io.File;
-import java.util.ArrayList;
+
 import weka.classifiers.functions.MultilayerPerceptron;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.converters.CSVLoader;
+import weka.core.converters.ArffLoader;
+
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToNominal;
 
 @Service
 public class MlpService {    
 
-    public ResponseEntity<CalculoDTO> calcular(List<String> matriz) throws Exception {    
-
+    public ResponseEntity<CalculoDTO> calcular(List<String> board) throws Exception {
+        System.out.println("dentro do método calcular matriz: " + board);
+    
         // Carrega o dataset de treino
-        CSVLoader loader = new CSVLoader();
-        loader.setFieldSeparator(",");
-        loader.setSource(new File("src/treino_balanceado.csv"));
-        Instances data = loader.getDataSet();
-
+        String filePath = "C:\\Users\\leona\\Desktop\\ia-back2\\ia-t1-back-main\\src\\treino_balanceado_mlp.arff";
+        ArffLoader loader = new ArffLoader();
+        loader.setFile(new File(filePath));
+        System.out.println("arquivo: " + loader);
+    
+        Instances instances = loader.getDataSet();
+        instances.setClassIndex(instances.numAttributes() - 1);
+    
+        System.out.println("loader conseguiu buscar o dataset: " + loader + "\n");
+    
         // seta classe de atributos
-        data.setClassIndex(data.numAttributes() - 1);
-
-        //codificação nos atributos categóricos.
-        int[] categoricalIndices = {0, 1, 2, 4, 5, 6, 8};
+        instances.setClassIndex(instances.numAttributes() - 1);
+    
+        // codificação nos atributos categóricos
+        int[] categoricalIndices = {0, 1, 2, 3, 4, 5, 6, 7, 8};
         for (int i : categoricalIndices) {
-            weka.filters.unsupervised.attribute.StringToNominal filter = new weka.filters.unsupervised.attribute.StringToNominal();
+            StringToNominal filter = new StringToNominal();
             filter.setAttributeRange(Integer.toString(i + 1));
-            filter.setInputFormat(data);
-            data = weka.filters.Filter.useFilter(data, filter);
+            filter.setInputFormat(instances);
+            instances = Filter.useFilter(instances, filter);
         }
-
-        //treina o classificador mlp
+    
+        // treina o classificador MLP
         MultilayerPerceptron mlp = new MultilayerPerceptron();
         mlp.setHiddenLayers("10");
-        mlp.setLearningRate(0.1);
+        mlp.setLearningRate(0.5);
         mlp.setTrainingTime(2000);
-        mlp.buildClassifier(data);
+        mlp.buildClassifier(instances);
+    
+        // cria uma instância a partir do tabuleiro do jogo
+        Instance instance = createInstanceFromBoard(board, instances);
+    
+        // faz a previsão usando o classificador treinado
+        double prediction = mlp.classifyInstance(instance);
 
-        //Faz previsões no input da matriz usando o classificador treinado 
-        double[] predictions = new double[matriz.size()];
-        for (int i = 0; i < matriz.size(); i++) {
-            Instance instance = createInstanceFromMatriz(matriz, data);
-            predictions[i] = mlp.classifyInstance(instance);
-        }
+        System.out.println("valor previsto pelo algoritmo: " + prediction + "\n");
 
         // retorna o resultado
-        List<Double> result = new ArrayList<>();
-        for (double prediction : predictions) {
-            result.add(prediction);
-        }
-
+        StatusEnum status = prediction == 0.0 ? StatusEnum.CONTINUA : StatusEnum.ENCERRAR;
         return ResponseEntity.ok(
                 CalculoDTO.builder()
-                        .status(StatusEnum.CONTINUA)                        
+                        .status(status)
                         .build());
     }
+    
+    
 
-    private Instance createInstanceFromMatriz(List<String> matriz, Instances data) {
+    private Instance createInstanceFromBoard(List<String> board, Instances data) {
         Instance instance = new DenseInstance(data.numAttributes());
         instance.setDataset(data);
-        for (int i = 0; i < matriz.size(); i++) {
-            instance.setValue(i, matriz.get(i));
-        }
-        return instance;
-    }
     
+        // cria os atributos da instância
+        int attributeIndex = 0;
+        for (String cell : board) {
+            switch (cell) {
+                case "x":
+                    instance.setValue(attributeIndex, 1.0);
+                    break;
+                case "o":
+                    instance.setValue(attributeIndex, 2.0);
+                    break;
+                default:
+                    instance.setValue(attributeIndex, 0.0);
+            }
+            attributeIndex++;
+        }
+    
+        return instance;
+    }        
 }
 
 
