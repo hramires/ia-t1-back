491393d174c635ccfa98af04ed28f1c02398ac15
yazuc <51753599+yazuc@users.noreply.github.com>
2023-05-11 15:52:16 -0300

    controller knn com chamada p/ mlp

--- src/main/java/com/pucrs/iat1back/knn/controller/KnnController.java
+++ src/main/java/com/pucrs/iat1back/knn/controller/KnnController.java
@@ -2,6 +2,8 @@ package com.pucrs.iat1back.knn.controller;
 
 import com.pucrs.iat1back.dto.CalculoDTO;
 import com.pucrs.iat1back.knn.service.KnnService;
+import com.pucrs.iat1back.mlp.service.MlpService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +20,19 @@ public class KnnController {
     @Autowired
     private KnnService knnService;
 
+    @Autowired
+    private MlpService mlpService;
+
     @PostMapping("/calcular")
     public ResponseEntity<CalculoDTO> calcularKnn(@RequestBody List<String> matriz) throws IOException {
         return knnService.calcular(matriz);
+    }
+
+    @PostMapping("/calcularMLP")
+    public ResponseEntity<CalculoDTO> calcularMlp(@RequestBody List<String> matriz) throws Exception {
+        System.out.println("matriz: " + matriz);
+
+        return mlpService.calcular(matriz);
     }
 
 }
